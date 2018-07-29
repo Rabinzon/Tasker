6a852f75dab96e4c4323d687597c69061f892272
rabinzon <ildarrazin@gmail.com>
2018-07-29 13:14:26 +0300

    fix(global) remove unnecessary styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/js/all';
-import './styles/form.css';
-import './styles/base.css';
 import './styles/navbar.css';
-
+import './styles/base.css';
