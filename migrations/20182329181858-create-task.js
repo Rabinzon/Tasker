b62d70c528c7ae99dc7b6347a791f64e98565b28
rabinzon <ildarrazin@gmail.com>
2018-08-13 08:38:40 +0300

    fix(migrations) task

--- migrations/20182329181858-create-task.js
+++ migrations/20182329181858-create-task.js
@@ -10,7 +10,7 @@ module.exports = {
       type: Sequelize.STRING,
     },
     description: {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
     },
     creatorId: {
       type: Sequelize.INTEGER,
