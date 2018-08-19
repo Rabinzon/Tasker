6b47ee55ee21230d11c9bd88500b949f42507787
rabinzon <ildarrazin@gmail.com>
2018-08-19 09:18:31 +0300

    chore: add favicon, add webpack plugins

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,18 @@
 import path from 'path';
 import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import CleanWebpackPlugin from 'clean-webpack-plugin';
 
 const mode = (process.env.NODE_ENV === 'test' && 'development') || process.env.NODE_ENV || 'development';
 
+const assetsPath = path.join(__dirname, 'public', 'assets');
+const faviconPath = path.join(__dirname, 'favicon');
+console.log(faviconPath);
 module.exports = {
   mode,
   entry: ['./src/index.js'],
   output: {
-    path: path.join(__dirname, 'public', 'assets'),
+    path: assetsPath,
     filename: 'main.js',
     publicPath: '/assets/',
   },
@@ -29,10 +34,12 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin([assetsPath]),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
     }),
+    new CopyWebpackPlugin([{ from: faviconPath, to: path.join(assetsPath, 'favicon'), toType: 'dir' }]),
   ],
 };
